623d9a8f494bf8d13a28e3bd03ade5383afb5080
vinh le <lenguyenngocmi@gmaiil.com>
2020-10-22 12:32:04 +0700

    Add project files.

---
+++ Bai3/Controllers/BannerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bai3.Models;
+
+namespace Bai3.Controllers
+{
+    public class BannerController : Controller
+    {
+        Bai3Entities db = new Bai3Entities();
+        // GET: Banner
+       
+            public ActionResult Index()
+            {
+                return View();
+            }
+            public ActionResult HeaderBanner()
+            {
+                var headerBanner = db.Banner.Where(c => c.Name == "Header").FirstOrDefault();
+                return PartialView(headerBanner);
+            }
+            public ActionResult LeftBanner()
+            {
+                var leftBanner = db.Banner.Where(c => c.Name == "Left").FirstOrDefault();
+                return PartialView(leftBanner);
+            }
+            public ActionResult RightBanner()
+            {
+                var rightBanner = db.Banner.Where(c => c.Name == "Right").FirstOrDefault();
+                return PartialView(rightBanner);
+            }
+        }
+   }
---
+++ Bai3/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bai3.Models;
+namespace Bai3.Controllers
+{
+    public class MenuController : Controller
+    {
+        Bai3Entities db = new Bai3Entities();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            var menus = db.Menu;
+            return PartialView(menus);
+        }
+    }
+}
